0a414bb8ed578641e1c2e152a7dc1348b8b9d95f
PRIYANKAS3110 <priyankasekarjaya@gmail.com>
2024-12-11 09:35:46 +0530

    second

--- lib/db.ts
+++ lib/db.ts
@@ -1,10 +1,15 @@
 import mysql from 'mysql2/promise';
+import dotenv from 'dotenv';
 
+// Load environment variables from the .env file
+dotenv.config();
+
+// Create a MySQL connection pool using environment variables
 const db = mysql.createPool({
-  host: 'localhost',       // Replace with your host
-  user: 'root',            // Replace with your username
-  password: 'Priyanka03#',            // Replace with your password
-  database: 'employee_management', // Replace with your database name
+  host: process.env.DB_HOST,          // Get from .env
+  user: process.env.DB_USER,          // Get from .env
+  password: process.env.DB_PASSWORD,  // Get from .env
+  database: process.env.DB_NAME,      // Get from .env
 });
 
 export default db;
